ac0d7ce4e227986301fa54d5e6c1a5e7e56ed300
Stawicki2137 <105315201+Stawicki2137@users.noreply.github.com>
2026-05-28 12:35:33 +0200

    stag2 kolejka FIFO

--- herman/sop-mag.c
+++ herman/sop-mag.c
@@ -42,10 +42,76 @@ typedef struct{
     uint16_t x;
     uint16_t y;
 }cast_t;
-/*
 typedef struct{
-    cast_t
-}queue_fifo_t; */
+    cast_t cast_queue[MAX_QUEUE]; // tablica z zakleciami
+
+    int head; //skad zdejmujemy
+    int tail; //gdzie dokladamy
+
+    pthread_mutex_t mutex;
+
+    sem_t slots_sem; //liczba wolnych miejsc
+    sem_t casts_sem; //liczba zaklec w kolejce
+
+}queue_fifo_t; 
+
+void init_queue(queue_fifo_t*queue){
+    queue->head = 0;
+    queue->tail = 0;
+
+    if(pthread_mutex_init(&queue->mutex,NULL)!=0)
+    ERR("pthread_mutex_init");
+    
+    if(sem_init(&queue->slots_sem,0,MAX_QUEUE)<0)
+    ERR("sem_init: slots_sem");
+
+    if(sem_init(&queue->casts_sem,0,0)<0)
+    ERR("sem_init: slots_sem");
+}
+
+void pop_queue(queue_fifo_t* queue, cast_t* out_cast){
+    while(sem_wait(&queue->casts_sem)==-1){
+        if(errno==EINTR)
+        continue;
+        else {
+            ERR("sem_wait:casts_sem");
+        }
+    }
+
+    pthread_mutex_lock(&queue->mutex);
+    *out_cast = queue->cast_queue[queue->head];
+    queue->head = (queue->head+1)%MAX_QUEUE;
+    pthread_mutex_unlock(&queue->mutex);
+
+    if(sem_post(&queue->slots_sem)<0)
+    ERR("sem_post casts");
+    
+}
+
+
+int push_queue(queue_fifo_t* queue, cast_t cast){
+    
+    while(sem_trywait(&queue->slots_sem)==-1){
+        if(errno==EINTR)
+        continue;
+
+        if(errno==EAGAIN){
+            return -1; //kolejka pelna to zwrocilo mi EAGAIN
+        }
+
+        ERR("sem_trywait:slots");
+    }
+
+    pthread_mutex_lock(&queue->mutex);
+    queue->cast_queue[queue->tail]=cast;
+    queue->tail = (queue->tail+1) % MAX_QUEUE;
+    pthread_mutex_unlock(&queue->mutex);
+
+    if(sem_post(&queue->casts_sem)<0)
+    ERR("sem_post casts");
+
+    return 0;
+}
 void usage(char* name)
 {
     printf("%s <in_port>\n", name);
@@ -63,12 +129,33 @@ int validate(int spell_index, int x, int y){
     return 0;
     
 }
-void doServer(int server_fd){
-    int counter = 0;
-    char recvbuff[MAX_NAME_LENGTH+2];
+void *worker_routine(void*arg){
+    queue_fifo_t* queue = arg;
+    cast_t out;
     while(1){
-        if(counter>=MAX_MESSAGES)
-        break;
+
+    pop_queue(queue,&out);
+    ms_sleep(FAMILIAR_DELAY);
+
+    //wypisac i elo
+    printf("[Cast] %s onto %d %d\n",spell_names[out.spell_index],out.x,out.y);
+    }
+   
+    return NULL;
+}
+void doServer(int server_fd){
+    char recvbuff[MAX_NAME_LENGTH+2];
+
+    queue_fifo_t queue;
+    init_queue(&queue);
+
+    pthread_t threads[THREAD_COUNT];
+    for(int i=0; i<THREAD_COUNT; i++){
+        if(pthread_create(&threads[i],NULL,worker_routine,&queue)!=0)
+        ERR("pthread_create");
+    }
+
+    while(1){
 
         memset(recvbuff,0,sizeof(recvbuff));
 
@@ -79,6 +166,18 @@ void doServer(int server_fd){
         char type;
         type = recvbuff[0];
         
+        /*
+        inny parser z chata 
+        uint16_t net_spell, net_x, net_y;
+
+memcpy(&net_spell, recvbuff + 2, sizeof(uint16_t));
+memcpy(&net_x,     recvbuff + 4, sizeof(uint16_t));
+memcpy(&net_y,     recvbuff + 6, sizeof(uint16_t));
+
+uint16_t spell_index = ntohs(net_spell);
+uint16_t x_target = ntohs(net_x);
+uint16_t y_target = ntohs(net_y);
+        */
         uint16_t spell_index = -1;
         uint16_t x_target = -1;
         uint16_t y_target = -1;
@@ -88,6 +187,7 @@ void doServer(int server_fd){
             break;
 
             case 'c':
+            {
             int ret = sscanf(recvbuff+sizeof(char)*2,"%hu %hu %hu",&spell_index,&x_target,&y_target);
             if(ret!=3){
                 printf("Podaj poprawne zaklecie: spell_index x_target y_target\n");
@@ -98,10 +198,20 @@ void doServer(int server_fd){
                 continue;
             }
 
-            // dobra cos tam zvalidowane to teraz komunikat:
 
-            printf("[Cast] %s onto %d %d\n",spell_names[spell_index],x_target,y_target);
+            //teraz wrzucam na kolejke 
+            cast_t cast;
+            cast.spell_index = spell_index;
+            cast.x = x_target;
+            cast.y = y_target;
+
+            if(push_queue(&queue,cast)<0)
+            printf("Brak miejsca w kolejce\n");
+
+            
             break;
+
+        }
 
             case 'q':
             printf("[Quit] Someone quit. Goodbye!\n");
@@ -113,7 +223,10 @@ void doServer(int server_fd){
             break;
         }
 
-        counter++;
+        
+    }
+     for(int i=0; i<THREAD_COUNT; i++){
+        pthread_join(threads[i],NULL);
     }
 }
 int main(int argc, char** argv)
